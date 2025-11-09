26a63b6ad8b141cb2d3aef86b73599ce4a3d785b
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-11-09 17:54:59 +0700

    Add particles to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,13 +4,106 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import FeatureProject from "../FeatureProject";
 
 import styles from "../../Hero.module.css";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function Home(props) {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      // background: {
+      //   color: {
+      //     value: "#0d47a1",
+      //   },
+      // },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "top-left",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 70,
+        },
+        opacity: {
+          value: 0.7,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
   return (
     <>
-      <div
-        className={`${styles.background} flex items-end`}
-      >
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      )}
+      <div className={`${styles.background} flex items-end`}>
         <div className="container mx-auto lg:py-20">
           <div className="md:grid md:grid-cols-3 gap-x-20 ">
             <title>{`${props.sitename}`}</title>
